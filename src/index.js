013f2f149580828e0685410b904b901f7328f28b
Farhan Khan <97162698+farhanfaiyazkhan@users.noreply.github.com>
2023-02-18 16:14:22 +0530

    lect 403

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App'
+import * as pi from "./math"
+// import pi, {doublePI, triplePI} from './math';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <App />
+  <ul>
+    <li>{pi.default}</li>
+    <li>{pi.DoublePI()}</li>
+    <li>{pi.TriplePi()}</li>
+  </ul>
 );
---
+++ src/math.js
@@ -0,0 +1,13 @@
+const pi = 3.1415962;
+
+function DoublePI(){
+    return pi*2;
+}
+
+function TriplePi(){
+    return pi*3;
+}
+
+export default pi;
+
+export {DoublePI, TriplePi};
