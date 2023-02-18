5535eb0a085c69b56f2ddbb4620d83be0a7b9711
Farhan Khan <97162698+farhanfaiyazkhan@users.noreply.github.com>
2023-02-18 13:37:44 +0530

    Add style.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const img = "https://picsum.photos/200";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello World!</h1>
+  <div>
+    <h1 className="heading">Hello World!</h1>
+    <div>
+      <img src={img + "?grayscale"} alt="random-image"/>
+    </div>
+  </div>
 );
 
