00d5393a6f0a7f2da765d1647a02c16841c18c17
Farhan Khan <97162698+farhanfaiyazkhan@users.noreply.github.com>
2023-02-18 13:46:36 +0530

    react 399

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 
 const img = "https://picsum.photos/200";
 
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+}
+
+customStyle.color = "blue";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1 className="heading">Hello World!</h1>
-    <div>
-      <img src={img + "?grayscale"} alt="random-image"/>
-    </div>
+    <h1 style={customStyle}>Hello World!</h1>
   </div>
 );
 
