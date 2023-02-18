2911c81ac775c1c7237f1d6160ade5457d02d14d
Farhan Khan <97162698+farhanfaiyazkhan@users.noreply.github.com>
2023-02-18 14:47:31 +0530

    lec 401

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const time = new Date().getHours();
-
-let customStyle = {};
-
-let salutation;
-
-if(time < 12)
-{
-  salutation = "Morning";
-  customStyle.color = "red";
-} else if(time > 12 && time < 18) {
-  salutation = "Afternoon";
-  customStyle.color = "green";
-} else if(time > 18 && time < 24) {
-  salutation = "Evening";
-  customStyle.color = "blue";
-}
-
 root.render(
-  <div>
-    <h1 style={customStyle}>Good {salutation}!</h1>
-  </div>
+  <App />
 );
-
